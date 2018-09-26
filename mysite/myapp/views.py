2c8acdde3b60a65e91ab41e454f5ab760bfb15d5
yash maheshwari <yashmahes@gmail.com>
2018-09-26 08:55:29 +0530

    added gitignore

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+
+from .models import Language
+from .serializers import LanguageSerializer
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+
+
